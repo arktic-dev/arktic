b499eabbffe48fc47011317320255b7399f40782
NicholasPiano <nicholas.d.piano@gmail.com>
2015-03-13 17:18:30 +0000

    finished

--- woot/apps/distribution/management/commands/jobs.py
+++ woot/apps/distribution/management/commands/jobs.py
@@ -1,5 +1,6 @@
 #django
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 
 #local
 from apps.distribution.models import Job
@@ -21,7 +22,7 @@ class Command(BaseCommand):
     available_transcription_set = Transcription.objects.filter(job__is_available=True)
 
     # delete available jobs
-    # available_jobs.delete()
+    available_jobs.delete()
 
     # separate transcriptions into yes, no and other
 
@@ -36,20 +37,21 @@ class Command(BaseCommand):
     yes_no = available_transcription_set.filter(utterance__contains='no ').filter(utterance__contains='nope ').filter(utterance__contains='yes ').filter(utterance__contains='yeah ')
 
     # list of pk
-    pk_list = [t.pk for t in other] + [t.pk for t in yes_wo_no] + [t.pk for t in no_wo_yes] + [t.pk for t in yes_no]
+    pk_list = [t.pk for t in yes_wo_no] + [t.pk for t in no_wo_yes] + [t.pk for t in yes_no]
 
-    transcription_set = Transcription.objects.filter(pk__in=pk_list)
+    other_set = Transcription.objects.filter(pk__in=[t.pk for t in other]).order_by('utterance')
+    yes_no_set = Transcription.objects.filter(pk__in=pk_list)
 
+    total_pk = [t.pk for t in other_set] + [t.pk for t in yes_no_set]
 
-    # print('creating jobs...')
-    # filter_set = self.transcriptions.filter(is_available=True).order_by('grammar__name').order_by('utterance')
-    # counter = filter_set.count() - 1 if filter_set.count() else 0
-    # while counter:
-    #   print('available: %d'%(counter), end='\r')
-    #   job = self.jobs.create(client=self.client, id_token=generate_id_token(Job))
-    #   lower_bound = counter-settings.NUMBER_OF_TRANSCRIPTIONS_PER_JOB if counter>=settings.NUMBER_OF_TRANSCRIPTIONS_PER_JOB else 0
-    #   job_set = filter_set[lower_bound:counter]
-    #   job.get_transcription_set(job_set)
-    #   job.save()
-    #   counter = lower_bound
-    # print('available: 0')
+    print('creating jobs...')
+    counter = len(all_pk) - 1 if len(all_pk) else 0
+    while counter:
+      print('available: %d'%(counter), end='\r')
+      job = self.jobs.create(client=self.client, id_token=generate_id_token(Job))
+      lower_bound = counter-settings.NUMBER_OF_TRANSCRIPTIONS_PER_JOB if counter>=settings.NUMBER_OF_TRANSCRIPTIONS_PER_JOB else 0
+      job_set = total_pk[lower_bound:counter]
+      job_t_set = Transcription.objects.filter(pk__in=job_set)
+      job.get_transcription_set(job_t_set)
+      job.save()
+      counter = lower_bound
