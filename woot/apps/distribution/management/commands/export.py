0d4880658dc9fd51cace62878d1eccfecc0004b4
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-04 23:27:14 +0100

    two brief tests

--- woot/apps/distribution/management/commands/export.py
+++ woot/apps/distribution/management/commands/export.py
@@ -64,11 +64,11 @@ class Command(BaseCommand):
         number_of_transcriptions = project.transcriptions.count()
         revisions = Revision.objects.filter(transcription__project=project)
 
-        with open(os.path.join(client_root, '{}.csv'.format(project.name)), 'w+') as csv_file:
-          for i, revision in enumerate(revisions):
-            # print('Exporting {}/{}...     '.format(i+1, revisions.count()), end='\r' if i+1<revisions.count() else '\n')
-            # csv_file.write('{},{},{}\n'.format(i, revision.transcription.audio_file.name, revision.utterance))
-            print('{},{},{}'.format(i, revision.transcription.audio_file.name, revision.utterance))
+        # with open(os.path.join(client_root, '{}.csv'.format(project.name)), 'w+') as csv_file:
+        for i, revision in enumerate(revisions):
+          # print('Exporting {}/{}...     '.format(i+1, revisions.count()), end='\r' if i+1<revisions.count() else '\n')
+          # csv_file.write('{},{},{}\n'.format(i, revision.transcription.audio_file.name, revision.utterance))
+          print('{},{},{}'.format(i, revision.transcription.audio_file.name, revision.utterance))
 
       else:
         print('Exporting all projects from client {}'.format(client_name))
