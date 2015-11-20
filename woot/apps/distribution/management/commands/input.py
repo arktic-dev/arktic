8bab9a16fc8f7527c4d32ca56405b69f3a1da3ff
NicholasPiano <nicholas.d.piano@gmail.com>
2015-11-20 17:54:01 +0000

    test updated input

--- woot/apps/distribution/management/commands/input.py
+++ woot/apps/distribution/management/commands/input.py
@@ -41,11 +41,19 @@ class Command(BaseCommand):
 
         # 4. list all files in the project directory and create transcriptions
         audio_root = os.path.join(root, client_name, project_name)
-        audio_files = [f for f in os.listdir(os.path.join(audio_root)) if '.wav' in f]
+				audio_files = [f for d,ds,fs in os.walk(audio_root) for f in fs if '.wav' in f] # double list of filenames to single list
+				relfile_name = [f for f in os.listdir(audio_root) if '.csv' in f][0] # return single item
+				relfile_path = os.path.join(root, client_name, project_name, relfile_name)
+
+				relfile_dictionary = {}
+				with open(relfile_path, 'r') as open_relfile:
+					# super complicated dictionary comprehension
+					relfile_dictionary = {os.path.basename(path):{'utterance':utterance, 'path':path} for line in open_relfile.readlines() for path, utterance in line.rstrip().split('|')}
 
         for i, audio_file in enumerate(audio_files):
           if project.transcriptions.filter(audio_file='audio/{}'.format(audio_file)).count()==0:
-            audio_file_path = os.path.join(audio_root, audio_file)
+            audio_file_path = relfile_dictionary[audio_file]['path']
+						utterance = relfile_dictionary[audio_file]['utterance']
             (seconds, rms_values) = process_audio(audio_file_path)
 
             max_rms = max(rms_values)
@@ -55,6 +63,7 @@ class Command(BaseCommand):
             with open(audio_file_path, 'rb') as open_audio_file:
               transcription, transcription_created = project.transcriptions.get_or_create(client=client,
                                                                                           id_token=generate_id_token('transcription','Transcription'),
+																																													utterance=utterance,
                                                                                           audio_time=seconds,
                                                                                           audio_rms=audio_rms,
                                                                                           audio_file=File(open_audio_file),
