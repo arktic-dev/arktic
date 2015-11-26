a4ff4212600441ed2d34e80fa5f4135c998b5b93
NicholasPiano <nicholas.d.piano@gmail.com>
2015-11-26 13:58:17 +0000

    oops

--- woot/apps/distribution/management/commands/make_users.py
+++ woot/apps/distribution/management/commands/make_users.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		user_access = join(settings.ACCESS_ROOT, settings.USER_ACCESS)
 		with open(user_access) as json_data:
-	    user_data = json.load(json_data)
+			user_data = json.load(json_data)
 
 			for user in user_data:
 				User.objects.create_superuser(user['email'], '1970-1-1', user['pwd'])
