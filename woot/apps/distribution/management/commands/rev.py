4c2ec8b306823f28143cc7d0187a5e14d5c80398
NicholasPiano <nicholas.d.piano@gmail.com>
2015-12-07 21:35:48 +0000

    rev command

---
+++ woot/apps/distribution/management/commands/rev.py
@@ -0,0 +1,48 @@
+# apps.distribution.command: input
+
+# django
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from django.core.files import File
+
+# local
+from apps.transcription.models import Revision
+
+# util
+import os
+import json
+import time
+from optparse import make_option
+
+# var
+spacer = ' '*10
+
+### Command
+class Command(BaseCommand):
+
+	option_list = BaseCommand.option_list + (
+
+		make_option('--fragment', # option that will appear in cmd
+			action='store', # no idea
+			dest='fragment', # refer to this in options variable
+			default='', # some default
+		),
+
+	)
+
+	args = ''
+	help = ''
+
+	def handle(self, *args, **options):
+		fragment = options['fragment']
+
+		if fragment:
+			matching_revisions = Revision.objects.filter(utterance__contains=fragment)
+
+			print('Fragment: {}'.format(fragment))
+			print('{} match{}'.format(matching_revisions.count(), 'es' if matching_revisions.count()!=1 else ''))
+			for revision in matching_revisions:
+				print('{}\t>>> {}\t>>> {}\t>>> {}\t>>> {}'.format(revision.pk, revision.user, revision.utterance, revision.transcription.utterance, os.path.basename(revision.transcription.audio_file.url)))
+
+			print('Fragment: {}'.format(fragment))
+			print('{} match{}'.format(matching_revisions.count(), 'es' if matching_revisions.count()!=1 else ''))
