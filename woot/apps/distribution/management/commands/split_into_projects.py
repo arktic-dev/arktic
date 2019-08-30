f6639ef1e49fde3cf4a12cf5b88eba2e38ac3860
Nicholas Piano <nicholas.d.piano@gmail.com>
2019-08-30 01:19:53 +0100

    WIP 3

--- woot/apps/distribution/management/commands/split_into_projects.py
+++ woot/apps/distribution/management/commands/split_into_projects.py
@@ -28,6 +28,8 @@ class Command(BaseCommand):
 
 		# 1. Get specific project
 		client_name = 'voxgen'
+		client = Client.objects.get(name=client_name)
+
 		project_name = 'extraction_0'
 		project = Project.objects.get(name=project_name)
 
@@ -46,7 +48,7 @@ class Command(BaseCommand):
 				with open(join(grammar_path, '{}.csv'.format(grammar_name)), 'r') as grammar_file:
 					for line in grammar_file.readlines():
 						tokens = line.rstrip().split('|')
-						audio_path = tokens[0]
+						audio_path = basename(tokens[0])
 						utterance = tokens[1]
 						relfile_dictionary.update({
 							audio_path: {
@@ -55,12 +57,34 @@ class Command(BaseCommand):
 							},
 						})
 
-		print(relfile_dictionary)
-
 		# 3. loop over transcriptions
-		# 4. fetch utterance and relfile_name based on dictionary
-		# 5. get_or_create new project and set project on transcription
-		# 6. if part of a job, create new job under project
+		for transcription in project.transcriptions.all():
+
+			print('>>>')
+
+			# 4. fetch utterance and relfile_name based on dictionary
+			audio_path = basename(transcription.audio_file.name)
+			print('PATH', audio_path)
+			grammar_details = relfile_dictionary[audio_path]
+
+			# 5. set utterance
+			utterance = grammar_details['utterance']
+			print('UTTERANCE', utterance)
+			# transcription.utterance = utterance
+
+			# 6. get_or_create new project and set project on transcription
+			grammar_name = grammar_details['grammar_name']
+			print('PROJECT', grammar_name)
+			# grammar_project, grammar_project_created = client.projects.get_or_create(name=grammar_name)
+
+			# 6. if part of a job, create new job under project
+			if transcription.job.all():
+				print('JOB', transcription.job.all())
+				print('REVISION', transcription.revisions.all())
+
+			print('<<<')
+
+
 		# 7. Add revision to new job if necessary
 
 		# mysql dump:
